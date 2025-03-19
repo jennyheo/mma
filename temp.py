79e9386090b4971f7cf044f59ba2562e85e8c1ef
jennyheo <hyj3589@gmail.com>
2025-03-19 20:53:39 +0900

    Add files via upload

--- temp.py
+++ temp.py
@@ -13,3 +13,9 @@ if st.button('현역입영대상'):
      st.write('1-3급은 현역입영대상입니다')
 if st.button('사회복무요원소집대상'):
      st.write('4급은 사회복무요원 소집 대상입니다')
+if st.button('전시근로역'):
+     st.write('5급은 전시근로역대상입니다')
+if st.button('병역면제'):
+     st.write('6급은 병역이 면제됩니다')
+if st.button('재검대상'):
+     st.write('7급 치유기간 이후에 다시 검사받으세요')
